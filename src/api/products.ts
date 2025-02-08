76b28a4bc43a8fa879bed06c22c314205f7e619e
yaniv080775 <yaniv080775@gmail.com>
2025-02-08 22:21:39 +0200

    fix url

--- src/api/products.ts
+++ src/api/products.ts
@@ -3,7 +3,7 @@ import { Product, promiseProduct } from "types"
 export const getProducts = async (): Promise<promiseProduct> => {
     try {
         //dispatch(loadProductsInProgress());
-        const response = await fetch('main.d32r2rewlbusrt.amplifyapp.com/api/products');
+        const response = await fetch('https://main.d32r2rewlbusrt.amplifyapp.com/api/products');
 
         const data = await response.json();
         return data;
