b728a6c13daba5ddf14c538e6d16c9a9c2f272d9
yaniv080775 <yaniv080775@gmail.com>
2025-02-10 08:59:12 +0200

    add api to em2

--- src/api/products.ts
+++ src/api/products.ts
@@ -4,7 +4,7 @@ export const getProducts = async (): Promise<promiseProduct> => {
     try {
         //dispatch(loadProductsInProgress());
         //yaniv
-        const response = await fetch('/api/products');
+        const response = await fetch('http://16.171.13.6/api/products');
 
         const data = await response.json();
         return data;
