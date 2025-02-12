d5900f4fed0a412e3634a627493dc8e1582a57ed
yaniv080775 <yaniv080775@gmail.com>
2025-02-12 08:52:54 +0200

    fix row card

--- src/layout/productCardRow.tsx
+++ src/layout/productCardRow.tsx
@@ -13,20 +13,20 @@ function ProductCardRow(props: { item: any }) {
                 history.push('/category/' +item.category+ '/' + item.uuid)
             })} >
             <div className="card product-row">
-                <div>
-                    <h6 className="cardTitle">{item.company}</h6>
-                    <p className="card-text"><small className="text-body-secondary">In stock: {item.amount}</small></p>
-                </div>
+
                 <div className="product-details">
-                            
+                    <h6 className="card-title details">{item.subcode || item.code}</h6>
                     <div className="card-text details" style={{ textAlign: "right" }}>
                         {descriptions}
                             {/*descriptions.map((description: string, index: number) =>
                                 <div key={index} className="text-body-secondary">{description}</div>
                             )*/}
-                        </div>
-                    <h6 className="card-title details">{item.subcode || item.code}</h6>
-                        </div>
+                        </div>                    
+                </div>
+                <div className='flex-row bottom-details'>
+                    <h6 className="cardTitle">{item.company}</h6>
+                    <div className="card-text">In stock: {item.amount}</div>
+                </div>
             </div>
         </div>
     );
